7d017a45d659ac55f326e30a2558b1ab327b84df
Lokesh Dhote <lokeshdhote2006@gmail.com>
2026-05-25 13:37:56 +0530

    Updated password generator UI

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const passwordBox=document.getElementById("password");
-const length=8;
-const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase="abcdefghijklmnopqrstuvwxyz";
-const numbers="0123456789"
-const symbol="@#$%^&*()~_+|{}[]></-=";
-const allChars=uppercase+lowercase+numbers+symbol;
+const passwordBox = document.getElementById("password");
+
+const length = 8;
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbol = "@#$%^&*()~_+|{}[]></-=";
+
+const allChars = uppercase + lowercase + numbers + symbol;
+
 function createPassword(){
-    let password="";
-    password+=uppercase[Math.floor(Math.random()*uppercase.length)];
-    password+=lowercase[Math.floor(Math.random()*lowercase.length)];
-    password+=numbers[Math.floor(Math.random()*numbers.length)];
-    password+=symbol[Math.floor(Math.random()*symbol.length)];
-    while (length>password.length){
-         password+=allChars[Math.floor(Math.random()*allChars.length)];
+
+    let password = "";
+
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+
     }
-    passwordBox.value=password;
+
+    passwordBox.value = password;
 }
-// function copyPassword(){
-//     passwordBox.select();
-//     document.execCommand("copy");
-// }
